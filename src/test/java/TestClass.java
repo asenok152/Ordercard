a00fc1f86c9b3887ae9f981c47068130049b4e42
Gasanova Anastasiya <iamgasanova@gmail.com>
2019-12-20 01:59:57 +0300

    make correct autotest

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -17,7 +17,7 @@ public class TestClass {
 
     @BeforeAll
     static void setDriverPath(){
-    System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
+    System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
     }
 
     @BeforeEach
@@ -35,12 +35,12 @@ public class TestClass {
     void someTest(){
         driver.get("http://localhost:9999");
      //   Thread.sleep(L 5000);
-        List<WebElement> elements = driver.findElements(By.className(input__control));
-        elements.get(0).sendKeys("Василий");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Петров Василий");
         elements.get(1).sendKeys("+79270000000");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена", text.trim());
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
